9870db0ff03ef31312523117651f6c91dce44d02
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-10-06 13:02:08 +0200

    Rename the project directory to get the right name

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 
 
+# Remove curly braces from the project directory name
+project_dir = Path.cwd()
+project_dir.rename(project_dir.parent / "{{ cookiecutter.project_slug }}")
+
+
+# Delete `requirements.txt` if not needed
 keep_requirements = {{ cookiecutter.pythontex or cookiecutter.minted }}
 
 if not keep_requirements:
